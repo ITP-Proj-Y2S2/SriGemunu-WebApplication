4fadd6276fbd234f99e4140138a1bc97aea01a6c
Isiwara Koralage <87434644+IsiwaraK@users.noreply.github.com>
2021-10-07 02:53:33 +0530

    sidebar update

--- FrontEnd/src/RestaurantComponents/Sidebar.js
+++ FrontEnd/src/RestaurantComponents/Sidebar.js
@@ -1,5 +1,8 @@
 import "../styles/Sidebar.css";
 import { AiFillHome } from "react-icons/ai"
+import {BiFoodMenu} from "react-icons/bi"
+import {AiOutlineInfoCircle} from "react-icons/ai"
+import {MdCall} from "react-icons/md"
 
 const Sidebar = ({show}) => {
 
@@ -8,9 +11,9 @@ const Sidebar = ({show}) => {
             <div className="list">
             <ul>
                 <li><a href="/restaurant"><AiFillHome /> Home</a></li>
-                <li><a href="/restaurant/menu">Menu</a></li>
-                <li><a href="/restaurant/about">About</a></li>
-                <li><a href="/restaurant/contact">Contact</a></li>
+                <li><a href="/restaurant/menu"><BiFoodMenu /> Menu</a></li>
+                <li><a href="/restaurant/about"><AiOutlineInfoCircle /> About</a></li>
+                <li><a href="/restaurant/contact"><MdCall /> Contact</a></li>
             </ul>
             </div>
 
